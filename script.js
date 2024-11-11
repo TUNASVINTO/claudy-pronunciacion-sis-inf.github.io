dcc798cac05951df4268c03dbbc5be776460ee87
TUNASVINTO <tunasvintou@gmail.com>
2024-11-10 23:49:10 -0400

    initil commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function function_name(argument) {
+    const palabras =[ "manzana", "perro", "cielo", "casa", "arbol"]
+    const palabraAleatoria = palabras[math.floor(math.random()*palabras.length)]
+    document.getElementById("palabra").inner Text = palabraAleatoria;
+}
